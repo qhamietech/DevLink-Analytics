0f518b9e8581ad9fafc7694fb3528147c5699eab
Qhamie <qhamietech@gmail.com>
2026-01-23 19:28:55 +0200

    fix: force global styles to load on login route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 // src/app/layout.tsx
-import "@/app/globals.css"; // Change "./globals.css" to "@/app/globals.css"
+import "@/app/globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
